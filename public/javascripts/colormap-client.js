2888b64b9b0be03c372c461a11ffd0792e020383
jdherman <jdherman8@gmail.com>
2013-08-04 20:40:07 -0400

    RGB lines work as object oriented things

--- public/javascripts/colormap-client.js
+++ public/javascripts/colormap-client.js
@@ -1,58 +1,48 @@
-var colormapPlot = function () {
+var colormapLine = function (r, numButtons, color) {
     
-    // Configure and create Raphael object
-    var W = 500,
-        H = 350,
-        r = Raphael("raph-holder", W, H);
-
-    var len = 5;
+    var W = r.width,
+        H = r.height;
         
-    function drawPath() {
+    this.drawPath = function() {
         var p = "M";
-        for (var i = 0; i < len; i++) {
+        for (var i = 0; i < numButtons; i++) {
             if(i > 0) p += "L";
-            p = p + [buttons.items[i].attr('cx'), buttons.items[i].attr('cy')];
+            p = p + [this.buttons.items[i].attr('cx'), this.buttons.items[i].attr('cy')];
         }
-        path.attr({path: p});
-        console.log(p);
+        this.path.attr({path: p});
     }
     
-    var color = "hsb(240°, 1, 1)",
-        buttons = r.set(),
-        path = r.path().attr({stroke: color, "stroke-width": 2});
+    this.buttons = r.set(),
+    this.path = r.path().attr({stroke: color, "stroke-width": 2});
 
-    for (i = 0; i < len; i++) {
+    for (i = 0; i < numButtons; i++) {
         
-        startX = 10 + (W - 60) / (len - 1) * i;
+        startX = 10 + (W - 60) / (numButtons - 1) * i;
         startY = H - 10 - (H - 20) / 100 * 50;
         
-        buttons.push(r.circle(startX, startY, 5).attr({fill: color, stroke: "none"}));
+        this.buttons.push(r.circle(startX, startY, 5).attr({fill: color, stroke: "none"}));
+        var parent = this;
 
-        buttons.items[i].mouseover(function () {
-            this.animate({r: 10}, 200);
-            this.node.style.cursor = "move";
-
-        }).mouseout(function () {
-            this.animate({r: 5}, 200);
-            this.node.style.cursor = "default";
-
-        }).drag(function (dx, dy) {
+        this.buttons.items[i].drag(function (dx, dy) { // "move" function
 
             tempY = this.attr("cy") + dy - (this.dy || 0);
-            
-            // if tempY > H - 10 ... or something
-            drawPath();
+            if(tempY > H - 10) tempY = H - 10;
+            else if (tempY < 10) tempY = 10;
+
+            parent.drawPath();
             this.attr({cy: tempY});
-            
             this.dx = dx;
             this.dy = dy;   
             r.safari();
 
         }, function () {
-            this.dx = this.dy = 0;
+            this.dx = this.dy = 0; // "start" function
         });
+
+        this.buttons.items[i].node.style.cursor = "move";
     }
     
-    drawPath();
+    this.drawPath();
 
+    return this;
 };
