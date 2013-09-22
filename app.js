73fcaeb5b3069fe3c52ce3a1b11d19d2a12de50c
jdherman <jdherman8@gmail.com>
2013-09-22 16:13:54 -0400

    added gallery. full thing working

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.engine('ejs', engine);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -30,7 +29,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index');
+
+  var presets = ['blues', 'dawn', 'dusk', 'fire', 'golds',
+				 'grays', 'greens', 'ice', 'kryptonite', 'purples',
+				 'redblue', 'reds', 'seashore', 'teals'];
+
+  res.render('index', {presets:presets});
 };
